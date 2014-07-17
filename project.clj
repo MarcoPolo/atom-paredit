de48882a27086f09d821728d809b20d10f79370e
Marco Munizaga <marco@marcopolo.io>
2014-07-16 20:18:58 -0700

    add default bidning for kill line

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2261"]
                  [com.cemerick/piggieback "0.1.3"]]
-  
+
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :plugins [[lein-cljsbuild "1.0.3"]]
@@ -19,4 +19,12 @@
                 :output-dir "lib/out"
                 :source-map "lib/paredit.js.map"
                 :target :nodejs
-                :optimizations :simple }}]})
+                :optimizations :simple}}
+             {:id "dev"
+              :notify-command ["terminal-notifier" "-message"]
+              :source-paths ["cljs"]
+              :compiler
+              {:output-dir "target/devout"
+                :output-to "target/paredit.js"
+               :target :nodejs
+               :optimizations :none}}]})

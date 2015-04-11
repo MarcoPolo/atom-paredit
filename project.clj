a3a6985ab2f51c130d875db75d2e3fda9c01df03
uzhne <uzhnetot@gmail.com>
2015-04-11 20:34:49 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2261"]
-                 [com.cemerick/piggieback "0.1.3"]]
+                 [org.clojure/clojurescript "0.0-3058"]
+                 [com.cemerick/piggieback "0.1.5"]]
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.5"]]
 
   :cljsbuild {
     :builds [{:id "paredit"
